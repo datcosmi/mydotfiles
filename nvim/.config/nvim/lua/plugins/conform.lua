2106e149dfd10ee5095e259921d1075081debc43
Iván García <cli.garcia@pm.me>
2026-06-30 16:35:55 -0600

    remove none_ls to rely just on conform

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,26 +1,29 @@
 return {
 	"stevearc/conform.nvim",
+	keys = {
+		{
+			"<leader>gf",
+			function()
+				require("conform").format({ async = true, lsp_fallback = true })
+			end,
+			mode = { "n", "v" },
+			desc = "Format buffer",
+		},
+	},
 	config = function()
 		require("conform").setup({
 			formatters_by_ft = {
 				lua = { "stylua" },
-
 				python = { "isort", "black" },
-
 				rust = { "rustfmt", lsp_format = "fallback" },
-
 				javascript = { "prettierd", "prettier", stop_after_first = true },
 				typescript = { "prettierd", "prettier", stop_after_first = true },
-
 				nix = { "alejandra" },
-
 				kdl = { "kdlfmt" },
+				cs = { "csharpier" },
+				sql = { "sqlfluff" },
 			},
-
-			format_on_save = {
-				timeout_ms = 2000,
-				lsp_fallback = true,
-			},
+			format_on_save = { timeout_ms = 2000, lsp_fallback = true },
 		})
 	end,
 }
